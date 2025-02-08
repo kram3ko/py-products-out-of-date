e9411c5cf29056b12759470ba7f30a330400fe0a
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-02-08 15:36:24 +0200

    Test to function outdated_products. Mocked. Fake date return

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+import datetime
+from unittest import mock
+
+from app.main import outdated_products
+
+
+def test_outdated_products() -> None:
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+    fake_date = datetime.date(2022, 2, 2)
+    with mock.patch("datetime.date") as mock_date:
+        mock_date.today.return_value = fake_date
+        assert outdated_products(products) == ["duck"]
